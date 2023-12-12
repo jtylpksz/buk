1d36257afa45e010cde99a0e3e1278731d7ff326
martínval11 <martinvaldez.dev@gmail.com>
2023-12-12 13:49:41 -0300

    use cases

---
+++ app/submitPost/form.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { Text, TextInput, Textarea, Container } from '@mantine/core';
+import { useFormState } from 'react-dom';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Toaster, toast } from 'sonner';
+
+import SubmitButton from '@/components/SubmitButton/SubmitButton';
+import { submitPostToDB } from '@/actions/createPost';
+
+const SubmitPostForm = () => {
+  const [username, setUsername] = useState('');
+  const router = useRouter();
+
+  const [broadcast, formAction]: any = useFormState(submitPostToDB, {
+    message: '',
+    success: false,
+    username: username,
+  });
+
+  useEffect(() => {
+    const username: any = localStorage.getItem('username');
+    setUsername(username);
+
+    if (broadcast.success && broadcast.message) {
+      toast.success(broadcast.message);
+      router.push('/');
+    } else if (!broadcast.success && broadcast.message) {
+      toast.error(broadcast.message);
+    }
+  }, [broadcast]);
+
+  return (
+    <>
+      <Container size="xs">
+        <form action={formAction}>
+          <Text size="xl">Create Post</Text>
+          <TextInput
+            mt="sm"
+            placeholder="Type here your title"
+            name="title"
+            autoComplete="off"
+            autoFocus
+            data-cy="postTitle"
+            required
+          />
+          <Textarea
+            mt="sm"
+            placeholder="Type here your content"
+            name="content"
+            autoComplete="off"
+            rows={5}
+            data-cy="postContent"
+            required
+          />
+          <input type="hidden" name="username" value={username} />
+
+          <SubmitButton
+            defaultValue="Create Post"
+            valueInRequest="Creating Post..."
+            fullWidth
+            mt="md"
+            dataCy="createPostButton"
+          />
+        </form>
+      </Container>
+      <Toaster richColors />
+    </>
+  );
+};
+
+export default SubmitPostForm;
--- app/submitPost/page.tsx
+++ app/submitPost/page.tsx
@@ -1,78 +1,16 @@
-'use client';
+import SubmitPostForm from './form';
+import { cookies } from 'next/headers';
 
-import { Text, TextInput, Textarea, Container } from '@mantine/core';
-import SubmitButton from '@/components/SubmitButton/SubmitButton';
-import { supabase } from '@/lib/supabaseClient';
-import { POSTS_TABLE } from '@/keys/keys';
-import { useRef } from 'react';
-import { Toaster, toast } from 'sonner';
+const userExist = () => {
+  return cookies().get('token') ? true : false;
+};
 
 const SubmitPost = () => {
-  const titleRef: any = useRef<HTMLInputElement>(null);
-  const contentRef: any = useRef<HTMLTextAreaElement>(null);
+  if (!userExist()) {
+    return <p>You need to be logged in to submit a post</p>;
+  }
 
-  const submitPostToDB = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const username = localStorage.getItem('username');
-    const title = titleRef.current.value;
-    const content = contentRef.current.value;
-
-    const post = {
-      author: username,
-      title,
-      content,
-    };
-
-    const { error } = await supabase.from(POSTS_TABLE).insert([post]);
-
-    if (error) {
-      console.log(error);
-      toast.error('Error creating post');
-      return;
-    }
-
-    toast.success('Post created successfully');
-    window.location.href = '/';
-  };
-
-  return (
-    <>
-      <Container size="xs">
-        <form onSubmit={submitPostToDB}>
-          <Text size="xl">Create Post</Text>
-          <TextInput
-            mt="sm"
-            placeholder="Type here your title"
-            name="title"
-            autoComplete="off"
-            autoFocus
-            ref={titleRef}
-            data-cy="postTitle"
-            required
-          />
-          <Textarea
-            mt="sm"
-            placeholder="Type here your content"
-            name="content"
-            autoComplete="off"
-            rows={5}
-            ref={contentRef}
-            data-cy="postContent"
-            required
-          />
-
-          <SubmitButton
-            defaultValue="Create Post"
-            valueInRequest="Creating Post..."
-            fullWidth
-            mt="md"
-            dataCy="createPostButton"
-          />
-        </form>
-      </Container>
-      <Toaster richColors />
-    </>
-  );
+  return <SubmitPostForm />;
 };
 
 export default SubmitPost;
