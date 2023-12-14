e7a01b63cccc7b54e5b5624716d67e312fb310b9
martinval11 <martinvaldez.dev@gmail.com>
2023-12-14 17:29:28 -0300

    delete comment option

--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -1,6 +1,16 @@
 'use client';
 
-import { Textarea, Text, Card, Button } from '@mantine/core';
+import {
+  Textarea,
+  Text,
+  Card,
+  Button,
+  Group,
+  Menu,
+  ActionIcon,
+  rem,
+} from '@mantine/core';
+import { IconDots, IconTrash } from '@tabler/icons-react';
 import { useEffect, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { Toaster, toast } from 'sonner';
@@ -19,6 +29,7 @@ export const revalidate = 0;
 
 const Comments = ({ data, id }: { data: CommentType[]; id: string }) => {
   const [username, setUsername] = useState('');
+  const [auth, setAuth] = useState(false);
   const [comments, setComments] = useState(data);
   const [pending, setPending] = useState(false);
 
@@ -54,11 +65,43 @@ const Comments = ({ data, id }: { data: CommentType[]; id: string }) => {
     }
   };
 
-  useEffect(() => {
-    const username = localStorage.getItem('username');
+  const deleteComment = async (event: any) => {
+    const commentID = event.currentTarget.id; // get the comment.id from the `delete` button
 
-    if (username) {
+    const confirmation = window.confirm(
+      'Are you sure you want to delete this comment?'
+    );
+
+    if (!confirmation) {
+      return;
+    }
+
+    // Filter all the comments except the one we want to delete
+    const filteredComments = comments.filter(
+      (comment) => comment.id !== commentID
+    );
+
+    const { error } = await supabase
+      .from(POSTS_TABLE)
+      .update({ comments: filteredComments })
+      .eq('id', id);
+
+    if (error) {
+      console.log(error);
+      toast.error('Something went wrong, please try again!');
+    }
+
+    toast.success('Comment deleted successfully!');
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    const username: any = localStorage?.getItem('username');
+    const auth: any = localStorage?.getItem('auth');
+
+    if (username && auth) {
       setUsername(username);
+      setAuth(auth);
     }
   }, []);
 
@@ -99,8 +142,41 @@ const Comments = ({ data, id }: { data: CommentType[]; id: string }) => {
 
       {comments?.map((comment: CommentType) => (
         <Card shadow="sm" padding="md" radius="md" mb={15} key={comment.id}>
-          <Text size="sm">{comment.author}</Text>
-          <Text size="md" c="dimmed">
+          <Card.Section withBorder inheritPadding py="xs">
+            <Group justify="space-between">
+              <Text fw={500}>{comment.author}</Text>
+              <Menu withinPortal position="bottom-end" shadow="sm">
+                <Menu.Target>
+                  <ActionIcon
+                    variant="subtle"
+                    color="gray"
+                    onClick={(event) => event.preventDefault()}
+                  >
+                    <IconDots style={{ width: rem(16), height: rem(16) }} />
+                  </ActionIcon>
+                </Menu.Target>
+
+                <Menu.Dropdown>
+                  {comment.author === username && auth ? (
+                    <Menu.Item
+                      id={comment.id}
+                      onClick={deleteComment}
+                      leftSection={
+                        <IconTrash
+                          style={{ width: rem(14), height: rem(14) }}
+                        />
+                      }
+                      color="red"
+                    >
+                      Delete
+                    </Menu.Item>
+                  ) : null}
+                </Menu.Dropdown>
+              </Menu>
+            </Group>
+          </Card.Section>
+
+          <Text size="md" c="dimmed" mt="sm">
             {comment.body}
           </Text>
         </Card>
